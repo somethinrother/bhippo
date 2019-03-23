e7bde54e1a1097eb770f4c0519df9ee8b5c04cf4
James Neilson <james@quandl.com>
2019-03-22 22:45:24 -0400

    Start prepping app for pods

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'bhippo',
+    podModulePrefix: 'bhippo/pods',
     environment,
     rootURL: '/',
     locationType: 'auto',
