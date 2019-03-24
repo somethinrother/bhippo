263eab27ebf11d8a16db29f3ef504826cb81364d
James Neilson <james@quandl.com>
2019-03-23 23:27:38 -0400

    Filter out misc results for now

--- app/pods/book/route.js
+++ app/pods/book/route.js
@@ -2,6 +2,9 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return this.store.findAll('genre', { include: 'books' });
+    return this.store.findAll('genre', { include: 'books' })
+                     .then(results => results.filter((genre) => {
+                       return genre.name !== 'misc';
+                     }))
   }
 });
